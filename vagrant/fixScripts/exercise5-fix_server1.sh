313e8cb5613ab8b9d1c707537c38cc0eb43365dd
hodayanir <64273842+hodayanir@users.noreply.github.com>
2020-05-03 21:04:06 +0300

    Update exercise5-fix_server1.sh

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+echo Host * >> ~/.ssh/config
+echo    StrictHostKeyChecking no >> ~/.ssh/config
+
+sudo ssh-keygen -t rsa -f /home/vagrant/.ssh/id_rsa -q -N ""
+
+sudo apt-get install sshpass
+sudo chown vagrant:vagrant /home/vagrant/.ssh/id_rsa*
+sshpass -p 'vagrant' scp /home/vagrant/.ssh/id_rsa.pub vagrant@server2:/home/vagrant

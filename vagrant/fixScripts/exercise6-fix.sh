6c96501db36c413b4d81546ff0ba6ceeadb0edbe
hodayanir <64273842+hodayanir@users.noreply.github.com>
2020-05-03 21:05:12 +0300

    Update exercise6-fix.sh

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+myArray=( "$@" )
+if [ $(hostname)=="server1" ]
+then
+        server=server2
+else
+        server=server1
+fi
+
+remoteLocation="${myArray[-1]}"
+
+for (( i=0; i< "${#myArray[@]} -1"; i++ ))
+do
+   scp ${myArray[$i]} "$server":$remoteLocation #| awk '{s+=$8} END {print s}'
+done
